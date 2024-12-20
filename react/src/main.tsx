ec060ec2fdcf39fa252de5c0718c15e055f85f9e
scorchedrice <woongveloper98@gmail.com>
2024-12-20 11:27:05 +0900

    fullpage.js => pageable

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -5,8 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div id="fullpage">
       <App />
-    </div>
   </StrictMode>,
 )
--- react/src/pages/IntroPage.tsx
+++ react/src/pages/IntroPage.tsx
@@ -1,9 +1,15 @@
 import Terminal from "../widgets/Terminal"
+import WallPaper from "../assets/wallpaper.jpg"
 
 export default function IntroPage() {
   return (
-    <div>
-      <Terminal/>
+    <div className="relative w-screen h-screen overflow-hidden">
+      <div className="absolute inset-0">
+        <img src={WallPaper} alt="wallPaper" className="w-full h-full object-cover" />
+      </div>
+      <div className="relative z-10 flex justify-center items-center">
+        <Terminal/>
+      </div>
     </div>
   )
 }
