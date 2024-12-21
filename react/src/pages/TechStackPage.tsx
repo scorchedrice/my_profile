5b5c895a8bf8dd8c7e06e6cc89c1ff8cb013ed98
scorchedrice <woongveloper98@gmail.com>
2024-12-21 18:00:51 +0900

    feat : techstack page (draft)

--- react/src/pages/TechStackPage.tsx
+++ react/src/pages/TechStackPage.tsx
@@ -1,16 +1,9 @@
-import Frontend from "../widgets/skills/Frontend.tsx";
-import Language from "../widgets/skills/Language.tsx";
-import Backend from "../widgets/skills/Backend.tsx";
-import TestFramework from "../widgets/skills/TestFramework.tsx"
+import Skills from "../widgets/skills/Skills.tsx";
 
 export default function TechStackPage() {
   return (
-    <div>
-      여기엔 기술 스택을 작성해요.
-      <Language/>
-      <Frontend/>
-      <Backend/>
-      <TestFramework/>
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <Skills/>
     </div>
   )
 }
