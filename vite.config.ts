8dae38d43f4f246f2fbbb6b91594c89cf5c8f0e8
scorchedrice <woongveloper98@gmail.com>
2024-12-23 16:00:24 +0900

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_profile/', // 경로를 명확히 지정
+  base: '/my_profile/',
   build: {
     outDir: 'dist',
     // 중요: assets 경로도 명시적으로 설정
     assetsDir: 'assets'
-  }
+  },
 })
