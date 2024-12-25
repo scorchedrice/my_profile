6f7aceed101c493c9bf2b5faa734742df4bf667b
scorchedrice <woongveloper98@gmail.com>
2024-12-25 09:26:26 +0900

    feat : new tech stack page

---
+++ src/pages/AboutMePage.tsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion"
+
+export default function AboutMePage() {
+  return (
+    <>
+      <motion.div className="h-[500dvh]" >
+        This is About Me Section
+      </motion.div>
+    </>
+  )
+}
