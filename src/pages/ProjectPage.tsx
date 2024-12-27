ad9b8f34f94d29d59325706f14895d5a8f6ebb34
scorchedrice <woongveloper98@gmail.com>
2024-12-27 20:07:26 +0900

    feat : project structure

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,7 +1,10 @@
+import ProjectWrapper from "../widgets/project/ProjectWrapper.tsx";
+
 export default function ProjectPage() {
   return(
     <div id="Projects" className="h-[300dvh] bg-blue-400 flex flex-col items-center">
       <h1 className="text-[50px] font-bold text-gray-700 ml-8">PROJECTS</h1>
+      <ProjectWrapper/>
     </div>
   )
 }
